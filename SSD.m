cd849ab8b72699ad298ce5fa00161c5a8518952b
kkostov5 <k_kostov95@abv.bg>
2018-03-16 18:27:46 +0000

    SSD change

--- SSD.m
+++ SSD.m
@@ -1,8 +1,6 @@
 function [ SSD ] = SSD( L, R )
     % Provides the sum of the squared difference between
-    % two matrices of the same size.
-    intL = int16(L);
-    intR = int16(R);
-    SSD = (intL - intR).^2;
+    % two support windows.
+    SSD = (L - R).^2;
     SSD= sum(sum(SSD));
 end
