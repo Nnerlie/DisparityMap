f1c11953fe35d08114cc56064d130a6145900bb6
kkostov5 <k_kostov95@abv.bg>
2018-03-16 17:21:58 +0000

    DISP_MAP update

--- DISP_MAP.m
+++ DISP_MAP.m
@@ -1,2 +1,13 @@
-function [  ] = DISP_MAP( ImageL, ImageR)
+function [ map ] = DISP_MAP( ImageL, ImageR)
+
+matrixL = imread(ImageL);
+matrixR = imread(ImageR);
+matrixDisp = matrixL;
+for row = 1: size(matrixL, 1)
+        for col = 1: size(matrixL, 2)
+           matrixDisp(row, col, 1)=PIXEL_DISP(matrixL(row,col),matrixR(row,col)); 
+        end
+end  
+    surf(double(matrixDisp));
+    imwrite(matrixDisp(:,:,1),"Result",'GIF');
 end
