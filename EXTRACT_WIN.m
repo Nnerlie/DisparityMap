3f84f2aa5f6ac5072ba1ff96225828a6e9a7f9bc
Krasimir Kostov <k_kostov95@abv.bg>
2018-03-28 15:24:14 +0100

    Fixing the problem with the bottom and right of the image.

--- EXTRACT_WIN.m
+++ EXTRACT_WIN.m
@@ -7,37 +7,37 @@ if row<=halfWindow % if pixel is in the top part of image
         window=image(1:windowSize,1:windowSize);
         topRow=1;
         topCol=1;
-    elseif size(image,2)-col<=halfWindow % if pixel is in the top right part of image
-        window=image(1:windowSize,size(image,2)-windowSize:size(image,2));
+    elseif size(image,2)-col<halfWindow % if pixel is in the top right part of image
+        window=image(1:windowSize,size(image,2)-windowSize+1:size(image,2));
         topRow=1;
-        topCol=size(image,2)-windowSize;
+        topCol=size(image,2)-windowSize+1;
     else % if pixel is in the top middle part of image
         window=image(1:windowSize,(col-(halfWindow)):(col+(halfWindow)));
         topRow=1;
         topCol=(col-(halfWindow));
     end
-elseif size(image,1)-row<=halfWindow % if pixel is in the bottom part of image
+elseif size(image,1)-row<halfWindow % if pixel is in the bottom part of image
     if col<=halfWindow% if pixel is in the bottom left part of image
-        window=image(size(image,1)-windowSize:size(image,1),1:windowSize);
-        topRow=size(image,1)-windowSize;
+        window=image(size(image,1)-windowSize+1:size(image,1),1:windowSize);
+        topRow=size(image,1)-windowSize+1;
         topCol=1;
-    elseif size(image,2)-col<=halfWindow % if pixel is in the bottom right part of image
-        window=image(size(image,1)-windowSize:size(image,1),size(image,2)-windowSize:size(image,2));
-        topRow=size(image,1)-windowSize;
-        topCol=size(image,2)-windowSize;
+    elseif size(image,2)-col<halfWindow % if pixel is in the bottom right part of image
+        window=image(size(image,1)-windowSize+1:size(image,1),size(image,2)-windowSize+1:size(image,2));
+        topRow=size(image,1)-windowSize+1;
+        topCol=size(image,2)-windowSize+1;
     else % if pixel is in the bottom middle part of image
-        window=image(size(image,1)-halfWindow:size(image,1),(col-halfWindow):(col+halfWindow));
-        topRow=size(image,1)-halfWindow;
+        window=image(size(image,1)-windowSize+1:size(image,1),(col-halfWindow):(col+halfWindow));
+        topRow=size(image,1)-windowSize+1;
         topCol=(col-halfWindow);
     end
 elseif col<=halfWindow % if pixel is in the middle left part of the image
     window=image((row-halfWindow):(row+halfWindow),1:windowSize);
     topRow=(row-halfWindow);
     topCol=1;
-elseif size(image,2)-col<=halfWindow % if pixel is in the middle right part of the image
-    window=image((row-halfWindow):(row+halfWindow),size(image,2)-windowSize:size(image,2));
+elseif size(image,2)-col<halfWindow % if pixel is in the middle right part of the image
+    window=image((row-halfWindow):(row+halfWindow),size(image,2)-windowSize+1:size(image,2));
     topRow=(row-halfWindow);
-    topCol=size(image,2)-windowSize;
+    topCol=size(image,2)-windowSize+1;
 else
     window=image((row-halfWindow):(row+halfWindow),(col-halfWindow):(col+halfWindow));
     topRow=(row-halfWindow);
