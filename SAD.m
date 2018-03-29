7e2c582bead6e104013ec9740c1c351ef5f8070a
ViiviPursiainen <ViiviPursiainen@computing.dundee.ac.uk>
2018-03-29 14:59:51 +0100

    More screenshots added

--- SAD.m
+++ SAD.m
@@ -1,6 +1,8 @@
 function [ SAD ] = SAD( L, R )
     % Provides the sum of absolute distances between
     % two support windows.
+    L = int16(L);
+    R = int16(R);
     SAD = (L - R);
     SAD= sum(sum(abs(SAD)));
 end
